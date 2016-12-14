d5a879261fe7447be743d812361faa5b00d3b9c6
Aiden <aiden@Darrens-MacBook-Air-2.local>
2016-12-14 13:25:00 -0800

    Add data structure to keep track of expenses and incomes and totals

--- app.js
+++ app.js
@@ -25,6 +25,17 @@ var budgetController = (function(){
 		this.value = value;
 	};
 
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
+
 })();
 
 // UI CONTROLLER
