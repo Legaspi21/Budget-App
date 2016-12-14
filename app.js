9ecd2ecfd758e6a36a0a42eede7807be034e6ab7
Aiden <aiden@Darrens-MacBook-Air-2.local>
2016-12-14 13:19:04 -0800

    Add income function constructor

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ var budgetController = (function(){
 		this.id = id;
 		this.description = description;
 		this.value = value;
-	}
+	};
+
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
 })();
 
