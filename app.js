8a06bedc3f4287b208be9cc29e9b3cb3bfb03570
Aiden <aiden@Darrens-MacBook-Air-2.local>
2016-12-14 13:08:08 -0800

    Pass DOMstrings to controller

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ var UIController = (function() {
 	var DOMstrings = {
 		inputType: '.add__type',
 		inputDescription: '.add__description',
-		inputValue: '.add__value'
-	}
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	};
 
 	return {
 		getInput: function() {
@@ -33,7 +34,10 @@ var UIController = (function() {
 				type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
 				description: document.querySelector(DOMstrings.inputDescription).value,
 				value: document.querySelector(DOMstrings.inputValue).value
-			}
+			};
+		},
+		getDOMstrings: function() {
+			return DOMstrings;
 		}
 	}
 
@@ -43,6 +47,8 @@ var UIController = (function() {
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
+
+	var DOM = UICtrl.getDOMstrings();
 
 	var ctrlAddItem = function() {
 		// Get field input data
@@ -55,10 +61,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// Calculate budget
 
 		// Display the budget on UI
-		console.log('it works')
+		console.log('it works');
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function(event){
 
